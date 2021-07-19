e61d511c3d66017db0018b000efa038f4cb6dab5
TomasGieco <86691163+TomasGieco@users.noreply.github.com>
2021-07-19 09:03:55 -0300

    Add files via upload

--- Tp dados/ascii.h
+++ Tp dados/ascii.h
@@ -1,7 +1,6 @@
 #ifndef ASCII_H_INCLUDED
 #define ASCII_H_INCLUDED
 
-void rectangulo(int x,int y,int ancho,int alto);
 void dado1(int x,int y);
 void dado2(int x,int y);
 void dado3(int x,int y);
@@ -205,10 +204,6 @@ gotoxy(x+ancho-6,y+alto/2);
 cout<<(char)4;
 gotoxy(x+ancho-2,y+alto/2);
 cout<<(char)4;
-gotoxy(x,y+alto);
-cout<<(char)200;
-gotoxy(x+ancho,y+alto);
-cout<<(char)188;
 gotoxy(x+2,y+alto-1);
 cout<<(char)4;
 gotoxy(x+2,y+alto-3);
@@ -218,36 +213,6 @@ cout<<(char)4;
 gotoxy(x+6,y+alto-3);
 cout<<(char)4;
 }
-
-
-
-
-void rectangulo(int x,int y,int ancho,int alto){
-    for(int i=x;i<=x+ancho;i++){
-        {gotoxy(i,y);
-        cout<<(char)205;}
-        }
-gotoxy(x,y);
-cout<<(char)201;
-gotoxy(ancho+x,y);
-cout<<(char)187;
-
-    for(int i=y;i<y+alto;i++){
-        gotoxy(x,i+1);
-        cout<<(char)186;
-        gotoxy(x+ancho,i+1);
-        cout<<(char)186;
-    }
-    for(int i=x;i<=x+ancho;i++){
-        gotoxy(i,y+alto);
-        cout<<(char)205;
-    }
-gotoxy(x,y+alto);
-cout<<(char)200;
-gotoxy(x+ancho,y+alto);
-cout<<(char)188;
-}
-
 
 
 #endif // ASCII_H_INCLUDED
